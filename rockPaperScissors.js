a780fde53f950230cbc6a1758c01437046fc3401
phunky-panda94 <victor.nim94@outlook.com>
2021-07-09 20:38:08 +1000

    updated

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,8 +1,22 @@
 function playRound(player, computer) {
 
-    if (player === 'rock' && computer === 'scissors') {
-        return 'Rock beats scissors! You win!';
+    // draw if equal
+    if (player === computer) {
+        return "It's a tie!";
     }
+
+    // win
+    if (player === rock && computer === scissors) {
+        
+    }
+    // rock - scissors
+    // paper - rock
+    // scissors - paper
+
+    // lose
+    // rock - paper -> lose
+    // paper - scissors -> lose
+    // scissors - rock -> lose
 
 }
 

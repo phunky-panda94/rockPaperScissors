e7a621c4239ad60f5215be4552a89c5c5c92819c
phunky-panda94 <victor.nim94@outlook.com>
2021-07-09 20:38:21 +1000

    updated

--- rockPaperScissors.spec.js
+++ rockPaperScissors.spec.js
@@ -1,6 +1,31 @@
 const { describe } = require('yargs');
 const playRound = require('./rockPaperScissors');
 
+test('tie if same', () => {
+    expect(playRound('rock', 'rock')).toBe("It's a tie!");
+});
+
 test('rock beats scissors', () => {
     expect(playRound('rock','scissors')).toBe('Rock beats scissors! You win!');
 });
+
+xtest('rock beats scissors', () => {
+    expect(playRound('scissors','rock')).toBe('Rock beats scissors! You lose')
+});
+
+xtest('paper beats rock', () => {
+    expect(playRound('paper','rock')).toBe('Paper beats rock! You win!');
+});
+
+xtest('paper beats rock', () => {
+    expect(playRound('rock','paper')).toBe('Paper beats rock! You lose!');
+});
+
+xtest('scissors beats paper', () => {
+    expect(playRound('scissors','paper')).toBe('Scissors beats paper! You win!');
+});
+
+xtest('scissors beats paper', () => {
+    expect(playRound('paper','scissors')).toBe('Scissors beats paper! You lose!');
+});
+
